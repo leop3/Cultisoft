19111a38640f46456a4d82d43b572c508511c7fa
leop3 <leoarancibia@hotmail.com>
2019-06-10 23:56:00 -0300

    Agrego para que funcione los rest

--- src/main/java/com/cultisoft/app/CultiSoftPdsApplication.java
+++ src/main/java/com/cultisoft/app/CultiSoftPdsApplication.java
@@ -3,11 +3,14 @@ package com.cultisoft.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories("com.cultisoft.repositories")
 @EntityScan("com.cultisoft.entities")
+@ComponentScan("com.cultisoft.controllers")
+@ComponentScan("com.cultisoft.service")
 public class CultiSoftPdsApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/cultisoft/controllers/CultivoRestController.java
+++ src/main/java/com/cultisoft/controllers/CultivoRestController.java
@@ -1,10 +1,13 @@
 package com.cultisoft.controllers;
 
-import java.util.List;
+import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.ModelMap;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,13 +20,22 @@ public class CultivoRestController {
 
 	@Autowired(required = true)
 	CultivoService cultivoService;
-	
-    @PostMapping(value="/getCultivos", headers="Accept=application/json")
-    public ModelMap mostrarTodosLosCultivos() {
-        List<Cultivo> lista=cultivoService.mostrarTodo();
-        ModelMap model = new ModelMap();
-        model.put("contenido", lista);
-        return model;
 
-    }
+	@PostMapping(path = "/getCultivos", headers = "Accept=application/json")
+	public Cultivo getCultivo() {
+		return new Cultivo(1, null, "clave", "nombre", "descripcion", new ArrayList());
+
+	}
+
+	@GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public Cultivo getById(@PathVariable("id") String id) {
+		String clave = id.equals("1") ? "clave de 1" : "esta no es clave de 1";
+		return new Cultivo(1, null, clave, "nombre", "descripcion", new ArrayList());
+
+	}
+
+	@PostMapping(path = "/insertCultivo")
+	public void insertarCultivo(@RequestBody Cultivo culti) {
+		cultivoService.agregar(culti);
+	}
 }
